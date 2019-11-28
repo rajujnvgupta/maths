4a56f31ffbd3c3fcd3d9a25c6b0232c7051a1cb7
rajujnvgupta <39230239+rajujnvgupta@users.noreply.github.com>
2019-11-28 22:20:07 +0530

    first commit

--- extendedEuclidean.cpp
+++ extendedEuclidean.cpp
@@ -20,16 +20,13 @@ int extendexdEuclidian(int a, int b, int &x, int &y){
 int32_t main() {
     int a;
     cin>>a;
-    bool isPossible = isPossibleModuloInverse(a, Mod);
-    
-    if(isPossible){
+
         int x = 0, y = 0;
         int ans = extendexdEuclidian(a, Mod, x, y);
         ///this print modulo inverse
         ///for negative value of inverse
         x = (x + Mod)%Mod; /// to make is positive
         cout<<x<<endl;
-    }
 
     return 0;
 }
